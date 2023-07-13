715e78b6525ac68f2bbdbf9024c204539d979395
박진우 <miriele75@gmail.com>
2023-07-13 16:43:30 +0900

    오류 수정

--- md_member/urls.py
+++ md_member/urls.py
@@ -5,5 +5,5 @@ app_name = "md_member"
 
 urlpatterns = [
     # path("Aaaa",       views.AaaaView.as_view(),       name="Aaaa"),    # view 추가하면 삭제하세요
-    path("login", views.LoginFormView.as_view(), name="login"),
+    # path("login", views.LoginFormView.as_view(), name="login"),
     ]
