f54ca629379c87c8fb1adf5b84e8f4dadcc0584a
leejurim <rty848@naver.com>
2023-07-14 16:01:39 +0900

    md_store/info 수정

--- md_store/views.py
+++ md_store/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.template import loader
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotFound
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from md_store.models import MdStor
 
 class InfoView(View):
     def get(self, request):
-        template = loader.get_template("md_store/info.html")
-        return HttpResponse(template.render()) 
+        stor_id = 244  # Set the desired stor_id here
+        store = MdStor.objects.get(stor_id=stor_id)
+        context = {
+            'dto': store
+        }
+        return render(request, 'md_store/info.html', context)
