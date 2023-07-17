9efc120bb6273c43c8f6e450cd26306b19fcdb52
minwoo <mwmskim@gmail.com>
2023-07-17 17:47:00 +0900

    templates/md_main/searchlist.html 생성 md_main/urls.py 경로추가 md_main/views.py searchview class생성 md_main/base.html수정 md_main/main.html 수정

--- md_main/views.py
+++ md_main/views.py
@@ -4,8 +4,10 @@ import logging
 from django import template
 from django.template import loader
 from django.http.response import HttpResponse
-from md_store.models import MdStorM
+from md_store.models import MdStorM, MdStor
 from md_combi.models import MdCombM
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 logger = logging.getLogger(__name__)
 
@@ -33,3 +35,31 @@ class MainView(View):
         return HttpResponse(template.render(context,request))
     def post(self,request):
         pass
+    
+class SearchView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return View.dispatch(self, request, *args, **kwargs)    
+    def get(self,request):
+        template = loader.get_template("md_main/searchlist.html")
+        count= MdStor.objects.all().count()
+        if count ==0:
+            context = {
+                "count":count,
+            }
+        searchtext=request.GET.get("searchtext")
+        sdtos = MdStor.objects.all()
+        # ddtos = MdStorM.objects.all().select_related("stor").select_related("menu").filter(menu__drnk_t=-1)
+        # tdtos = MdStorM.objects.all().select_related("menu").select_related("stor").filter(menu__dsrt_t=-1)
+        if searchtext:
+            sdtos = sdtos.filter(stor_name__icontains=searchtext)
+            # ddtos = ddtos.filter(stor__stor_name__icontains=searchtext)
+            # tdtos = tdtos.filter(stor__stor_name__icontains=searchtext)
+            count = sdtos.all().count()
+        context = {
+            "sdtos":sdtos,
+            "count":count,
+            # "ddtos":ddtos,
+            # "tdtos":tdtos,
+            }
+        return HttpResponse(template.render(context,request))
