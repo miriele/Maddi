bdce03430671c024af170fd7c6c1a9f0dfa2a73b
leejurim <rty848@naver.com>
2023-07-17 09:28:40 +0900

    static 파일 추가 및 매장 사진 출력

--- md_store/views.py
+++ md_store/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import render
-from django.views.generic.base import View
-from django.template import loader
-from django.http.response import HttpResponse, HttpResponseNotFound
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
+from django.views import View
 from md_store.models import MdStor
+from django.http.response import HttpResponse, HttpResponseNotFound
+from django.template import loader
 
-from django.conf import settings
-##################################################
-# 이주림
-# 지금은 비로 하드 코드로 박았지만..
-# 나중에 stor_id 넘겨 받는거로 수정할 것이다!!
-##################################################
+
 class StoreView(View):
     def get(self, request):
-        stor_id = 26234
-        store = MdStor.objects.get(stor_id=stor_id)
-        
-        if store.stor_t == 0:
-            stor_type = "개인"
-        else:
-            stor_type = "프랜차이즈"
-
-        context = {
-            'dto': store,
-            'stor_type': stor_type
-        }
-        return render(request, 'md_store/store.html', context)
+        stor_id = 19078
+        try:
+            store = MdStor.objects.get(stor_id=stor_id)
+            
+            if store.stor_t == 0:
+                stor_type = "개인"
+            else:
+                stor_type = "프랜차이즈"
+            
+            if store.stor_tel:
+                stor_tel = store.stor_tel
+            else:
+                stor_tel = "등록된 연락처가 없습니다"
+            
+            context = {
+                'dto': store,
+                'stor_type': stor_type,
+                'stor_tel': stor_tel
+            }
+            return render(request, 'md_store/store.html', context)
+        except MdStor.DoesNotExist:
+            return HttpResponseNotFound()
 
 
 class MenuView(View):
