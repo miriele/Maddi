875524437f67aea5dcb143a67428e8c20b087bdf
minwoo <mwmskim@gmail.com>
2023-07-17 14:50:06 +0900

    md_main/views.py 변경 templates/md_main/base.html변경 templates/md_main/main.html변경 templates/md_member/input.html변경 templates/md_member/login.html변경

--- md_main/views.py
+++ md_main/views.py
@@ -4,26 +4,31 @@ import logging
 from django import template
 from django.template import loader
 from django.http.response import HttpResponse
-from md_store.models import MdMenu, MdStor
+from md_store.models import MdStorM
+from md_combi.models import MdCombM
 
 logger = logging.getLogger(__name__)
 
 class MainView(View):
     def get(self,request):
         template = loader.get_template("md_main/main.html")
-        stor_name = MdStor.objects.only("stor_name")
-        tdtos = MdMenu.objects.filter(dsrt_t_id=-1)[:5]
-        ddtos = MdMenu.objects.exclude(dsrt_t_id=-1)[:5]
         memid = request.session.get("memid")
+        tdtos = MdStorM.objects.select_related("menu").filter(menu__dsrt_t=-1)[:5]
+        ddtos = MdStorM.objects.select_related("menu").filter(menu__drnk_t=-1)[:5]
+        rdtos = MdCombM.objects.select_related("comb")[:5]
         if memid:
             context = {
-                "memid":memid
+                "memid":memid,
+                "tdtos":tdtos,
+                "ddtos":ddtos,
+                "rdtos":rdtos,
+                
                 }
         else:
             context={
                 "tdtos":tdtos,
                 "ddtos":ddtos,
-                "stor_name":stor_name,
+                "rdtos":rdtos,
                 }
         return HttpResponse(template.render(context,request))
     def post(self,request):
