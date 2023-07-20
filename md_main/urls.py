6c6e78c543e9e4270ed6808736e0ec4648802cfb
박진우 <miriele75@gmail.com>
2023-07-20 14:49:01 +0900

    줄 맞추기

--- md_main/urls.py
+++ md_main/urls.py
@@ -1,6 +1,6 @@
 from django.urls.conf import path
 from md_main import views
 urlpatterns = [
-     path("main",views.MainView.as_view(),name="main"),
-     path("searchlist",views.SearchView.as_view(),name="searchlist"),
+     path("main",       views.MainView.as_view(),   name="main"),
+     path("searchlist", views.SearchView.as_view(), name="searchlist"),
      ]
