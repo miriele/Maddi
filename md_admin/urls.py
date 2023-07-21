1879af03533078cb86d6af81ca05ac73d41b8ea5
minwoo <mwmskim@gmail.com>
2023-07-21 11:42:26 +0900

    md_admin/urls.py path 설정 md_admin/views.py template 설정만완료 template 6개생성

--- md_admin/urls.py
+++ md_admin/urls.py
@@ -4,5 +4,10 @@ from md_admin import views
 app_name = "md_admin"
 
 urlpatterns = [
-    # path("Aaaa",       views.AaaaView.as_view(),       name="Aaaa"),    # view 추가하면 삭제하세요
+    path("userlist",  views.UserlistView.as_view(), name="userlist"),
+    path("userinfo",  views.UserinfoView.as_view(), name="userinfo"),
+    path("reviewlist",  views.ReviewlistView.as_view(), name="reviewlist"),
+    path("reviewinfo",  views.ReviewinfoView.as_view(), name="reviewinfo"),
+    path("sregistlist",  views.SregistlistView.as_view(), name="sregistlist"),
+    path("sregistinfo",  views.SregistinfoView.as_view(), name="sregistinfo"),
     ]
--- md_admin/views.py
+++ md_admin/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.http.response import HttpResponse
+from django.template import loader
+from md_member.models import MdUser
 
 # Create your views here.
+
+class UserlistView(View):
+    def get(self,request):
+        template = loader.get_template("md_admin/userlist.html")
+        count = MdUser.objects.count()   
+        users = MdUser.objects.select_related("user_g").only("user_id","user_name","user_g__user_g_name","user_reg_ts")    
+        context ={
+            "count":count,
+            "users":users,
+            }
+        return HttpResponse(template.render(context,request))
+
+class UserinfoView(View):
+    def get(self,request):
+        template = loader.get_template("md_admin/userinfo.html")
+        # id = request.GET["id"]
+        # users = MdUser.objects.select_related("user_g").get(user_id=id)
+        # queryset = users
+        context ={
+             }
+        return HttpResponse(template.render(context,request))
+    
+class ReviewlistView(View):
+    def get(self,request):
+        template = loader.get_template("md_admin/userlist.html")
+        context ={
+            }
+        return HttpResponse(template.render(context,request))
+    
+class ReviewinfoView(View):
+    def get(self,request):
+        template = loader.get_template("md_admin/userlist.html")
+        context ={
+            }
+        return HttpResponse(template.render(context,request))
+    
+class SregistlistView(View):
+    def get(self,request):
+        template = loader.get_template("md_admin/userlist.html")
+        context ={
+            }
+        return HttpResponse(template.render(context,request))
+    
+class SregistinfoView(View):
+    def get(self,request):
+        template = loader.get_template("md_admin/userlist.html")
+        context ={
+            }
+        return HttpResponse(template.render(context,request))   
