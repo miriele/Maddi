d74cd73559528c1a815c6d4b62d3b9a153334077
minwoo <mwmskim@gmail.com>
2023-07-21 20:36:29 +0900

    관리자 views.py 수정 템플릿 리뷰목록/리뷰상세 수정

--- md_admin/views.py
+++ md_admin/views.py
@@ -3,7 +3,8 @@ from django.views.generic.base import View
 from django.http.response import HttpResponse
 from django.template import loader
 from md_member.models import MdUser
-from md_review.models import MdReview
+from md_review.models import MdReview, MdTag, MdRevT
+from md_order.models import MdOrdr, MdOrdrM
 
 # Create your views here.
 
@@ -33,19 +34,38 @@ class ReviewlistView(View):
     def get(self,request):
         template = loader.get_template("md_admin/reviewlist.html")
         count = MdReview.objects.count()    #리뷰갯수
-        #리뷰리스트
-        rdtos = MdReview.objects.select_related("ordr")
-        #리뷰리스트에서 아이디,주문번호,매장명까지 출력성공 매장명 출력은 orm 수정이 필요함
+        
+        # 화면에 출력해줄 내용
+        # : 사용자명, 주문번호, 매장명, 리뷰등록일
+        # : md_ordr.user_id, md_ordr.ordr_id, md_stor.stor_name, md_review.rev_ts
+        
+        # select * from md_review;
+        # select * from md_ordr where ordr_id=1;
+        # select * from md_ordr_m where ordr_id=1;
+        # select * from md_stor_m where stor_m_id=15;
+        # select * from md_stor where stor_id=1;
+            
         context ={
             "count":count,
-            "rdtos":rdtos,
             }
         return HttpResponse(template.render(context,request))
     
 class ReviewinfoView(View):
     def get(self,request):
         template = loader.get_template("md_admin/reviewinfo.html")
+        revid = request.GET["revid"]
+        
+        #화면에 출력해줄 내용
+        #태그는 태그분류없이 나열
+        #리뷰이미지,    메장메뉴명,    리뷰별점,    태그명,    리뷰내용 
+        #md_review.rev_img,   md_stro_m.stor_m_name,    md_review.rev_star    md_tag.tag_name    md_review.rev_cont
+        
+        #SELECT * FROM md_stor_m WHERE stor_m_id = 15;
+        #SELECT rev_img,rev_star,rev_cont FROM md_review WHERE rev_id = 1;
+        #SELECT t.tag_name FROM md_tag t JOIN md_rev_t rt ON t.tag_id = rt.tag_id WHERE rt.rev_id = 1;
+        
         context ={
+            "revid":revid,
             }
         return HttpResponse(template.render(context,request))
     
