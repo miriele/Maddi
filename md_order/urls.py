5c2219cceae1a69ddf8e2053a15523a0d61f4342
leejurim <rty848@naver.com>
2023-07-21 10:58:59 +0900

    주문구현에 필요한 templates, url, static/image/bell.jpg 추가 view는 template 띄우기만 가능

--- md_order/urls.py
+++ md_order/urls.py
@@ -8,8 +8,13 @@ urlpatterns = [
          path( "cart", views.CartView.as_view(), name="cart" ),
          path( "order", views.OrderView.as_view(), name="order" ),
          path( "buck", views.BuckView.as_view(), name="buck" ),
+         
+         path( "buckdel", views.BuckDelView.as_view(), name="buckdel" ),
+         path( "buckordr", views.BuckOrdrView.as_view(), name="buckordr" ),
+         path( "buckordr", views.BuckOrdrView.as_view(), name="buckordr" ),
+         path( "orderlist", views.OrdrListView.as_view(), name="orderlist" ),
+         path( "ordersuc", views.OrdrSucView.as_view(), name="ordersuc" ),
+         path( "orderalert", views.OrdrAlertView.as_view(), name="orderalert" ),
+         path( "orderdone", views.OrdrDoneView.as_view(), name="orderdone" ),
        ]
-
-
-
 
