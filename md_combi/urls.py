0ae784e616b70c6e791b4c8a44d6a25126d49866
yhaehee <wpgml0211@naver.com>
2023-07-21 11:29:09 +0900

    추천조합/리뷰/즐겨찾기url, views, html

--- md_combi/urls.py
+++ md_combi/urls.py
@@ -4,5 +4,8 @@ from md_combi import views
 app_name = "md_combi"
 
 urlpatterns = [
-    path( "comblist",      views.CombListView.as_view(),       name="comblist"),
+    path( "comblist",      views.CombListView.as_view(),        name="comblist"),   # 추천 글목록 보기
+    path( "combwrite",     views.CombWriteView.as_view(),       name="combwrite"),  # 추천글 작성하기
+    path( "combd",         views.CombDView.as_view(),           name="combd"),      # 추천글 내용 보기
+    path( "combreply",     views.CombReplyView.as_view(),       name="combreply"),  # 댓글용 AJAX주소
     ]
--- md_review/views.py
+++ md_review/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.template import loader
+from django.views.generic.base import View
+from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from datetime import datetime
+import logging
 
-# Create your views here.
+# 로그
+logger = logging.getLogger( __name__ )
+
+# 리뷰 작성 폼
+class RevwriteView( View ):
+    def get(self, request ):
+        template = loader.get_template( "md_review/revwrite.html" )
+        context = {
+            }
+        return HttpResponse(template.render( context, request ) )
+    
+    
+# 리뷰 보기 폼>매뉴 정보페이지에 합체
+class ReviewView( View ):
+    def get(self, request ):
+        template = loader.get_template( "md_review/review.html" )
+        context = {
+            }
+        return HttpResponse(template.render( context, request ) )
