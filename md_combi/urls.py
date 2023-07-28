4320d324181539887ceef59bf78158374c4dafb3
yhaehee <wpgml0211@naver.com>
2023-07-28 12:06:03 +0900

    추천목록 작성 태그 값 받아와서 저장/ 추천목록  출력

--- md_combi/urls.py
+++ md_combi/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path( "comblist",      views.CombListView.as_view(),        name="comblist"),   # 추천 글목록 보기
     path( "combwrite",     views.CombWriteView.as_view(),       name="combwrite"),  # 추천글 작성하기
     path( "combd",         views.CombDView.as_view(),           name="combd"),      # 추천글 내용 보기
-    path( "clike",        views.CLikeView.as_view(),          name="clike"),      # 좋아요 
+    path( "clike",         views.CLikeView.as_view(),           name="clike"),      # 좋아요 
     ]
