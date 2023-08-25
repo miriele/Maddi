89b572775efa06093e0a626a1d8b6991f1875cc3
leejurim <rty848@naver.com>
2023-08-25 16:25:22 +0900

    오류수정

--- md_main/static/js/script_main.js
+++ md_main/static/js/script_main.js
@@ -349,3 +349,23 @@ function displayCenterInfo(result, status) {
 		//console.log(kakaomap.getLevel())
 	}    
 }
+
+
+window.onload = function() {
+	  const images = document.querySelectorAll('.img');
+	  let current = 0;
+
+	  function showSlide() {
+	    for (let i = 0; i < images.length; i++) {
+	      images[i].classList.remove('on');
+	    }
+	    current++;
+	    if (current > images.length) {
+	      current = 3;
+	    }
+	    images[current - 1].classList.add('on');
+	    setTimeout(showSlide, 2000);
+	  }
+
+	  showSlide();
+	};
